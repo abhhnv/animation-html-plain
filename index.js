fc8a37175a12db009d3ab3ba0438511cbe6fd019
abhhnv <140376982+abhhnv@users.noreply.github.com>
2023-11-08 15:03:46 +0530

    feat: add 3 animations

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-import Lottie from "lottie-web";
+import FirstDiv1Animation from './lottie/1.json'
+import FirstDiv2Animation from './lottie/scan_transition.json'
+import FirstDiv3Animation from './lottie/scan_loop.json'
 
+import lottieWeb from "lottie-web";
 // Add event listeners to the buttons
-document.getElementById("firstbtn").addEventListener("click", handleFirst);
+// document.getElementById("firstbtn").addEventListener("click", handleFirst);
 document.getElementById("secondbtn").addEventListener("click", handleSecond);
 document.getElementById("thirdbtn").addEventListener("click", handleThird);
 document.getElementById("fourthbtn").addEventListener("click", handleFourth);
@@ -10,6 +13,55 @@ document.getElementById("first").addEventListener("touchmove", firstDivMove);
 document.getElementById("second").addEventListener("touchmove", secondDivMove);
 document.getElementById("third").addEventListener("touchmove", thirdDivMove);
 document.getElementById("fourth").addEventListener("touchmove", fourthDivMove);
+
+const animations = [FirstDiv1Animation, FirstDiv2Animation, FirstDiv3Animation]
+
+/*
+scan-loop -> 3rd frame
+scan-transition - 2nd frame
+1 -> 1st frame
+*/
+
+
+function playNextAnimation() {
+    animation1.destroy()
+    var animation2 = lottieWeb.loadAnimation({
+        container: document.getElementById('lottieDivFirst'),
+        animationData: animations[1],
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        name: "Demo Animation",
+    });
+
+    animation2.play()
+    animation2.addEventListener('complete', playNextNextAnimation);
+    
+    function playNextNextAnimation() {
+        console.log("second complete")
+        animation2.destroy()
+        var animation3 = lottieWeb.loadAnimation({
+            container: document.getElementById('lottieDivFirst'),
+            animationData: animations[2],
+            renderer: 'svg',
+            loop: false,
+            autoplay: true,
+            name: "Demo Animation",
+        });
+        animation3.play()
+    }
+}
+
+var animation1 = lottieWeb.loadAnimation({
+    container: document.getElementById('lottieDivFirst'),
+    animationData: animations[0],
+    renderer: 'svg',
+    loop: false,
+    autoplay: true,
+    name: "Demo Animation",
+});
+
+animation1.addEventListener('complete', playNextAnimation);
 
 function firstDivMove() {
     handleFirst()
@@ -26,15 +78,6 @@ function thirdDivMove() {
 function fourthDivMove() {
     handleFourth()
 }
-
-
-Lottie.loadAnimation({
-    container: document.getElementById("testingg"),
-    renderer: "svg",
-    loop: true,
-    autoplay: true,
-    path: "/1.json"
-});
 
 function enableScrolling() {
     document.getElementById("maincontainer").style.overflow = "auto";

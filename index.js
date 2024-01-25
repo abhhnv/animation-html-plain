177f70cb73379f05987da0d474413d57854a6bd1
abhhnv <140376982+abhhnv@users.noreply.github.com>
2024-01-25 12:08:11 +0530

    fix: rename, lottie files

--- index.js
+++ index.js
@@ -1,275 +1,347 @@
-// first div
-import FirstDiv1Animation from './lottie/new/newnew/Features_Idle (1).json'
-import FirstDiv2Animation from './lottie/new/Scan&Pay_Transition.json'
-import FirstDiv3Animation from './lottie/new/Scan&Pay_Loop.json'
-// second div
-import SecondDivAnimation from './lottie/new/newnew/PopCoin (1).json'
-// third div
-import ThirdDivAnimation from './lottie/new/newnew/PopShop.json'
-// fourth div
-import FourthDivAnimation from './lottie/new/newnew/Rewards_2.json'
-// fifth div
-import FifthDiv1Animation from './lottie/new/newnew/Feature_ZoomOUT (1).json'
-import FifthDiv2Animation from './lottie/new/newnew/Feature_ZoomOUT_Idle.json'
-//lottie-web for handling lottie animations
 import lottieWeb from "lottie-web";
-// smooth scroll is not supported in safari, therefore a polyfill library
-import smoothScrollIntoView from "smooth-scroll-into-view-if-needed"; // Import the polyfill
+import One from './newlottiefiles/1.json'
+import Two from './newlottiefiles/2.json'
+import Three from './newlottiefiles/3.json'
+import Four from './newlottiefiles/4.json'
+import Fifth from './newlottiefiles/5.json'
+import Sixth from './newlottiefiles/6.json'
+// import Sixth_Loader from './newlottiefiles/Loader_Quadrant.json'
+import Sixth_Loader from './newlottiefiles/7-2.json'
 
-document.getElementById("first").addEventListener("touchmove", firstDivMove);
-document.getElementById("second").addEventListener("touchmove", secondDivMove);
-document.getElementById("third").addEventListener("touchmove", thirdDivMove);
-document.getElementById("fourth").addEventListener("touchmove", fourthDivMove);
-// skip buttons
-document.getElementById("skip-btn-first").addEventListener("click", handleRewardsBtn);
-document.getElementById("skip-btn-second").addEventListener("click", handleRewardsBtn);
-document.getElementById("skip-btn-third").addEventListener("click", handleRewardsBtn);
-document.getElementById("skip-btn-fourth").addEventListener("click", handleRewardsBtn);
+import Eight from './newlottiefiles/Supercard-3.json'
+import Nine from './newlottiefiles/Shop_quadrant2-2.json'
+import Ten from './newlottiefiles/10.json'
+import Eleven from './newlottiefiles/11.json'
+import Twelve from './newlottiefiles/12.json'
+import Thirteen from './newlottiefiles/13.json'
+import Fourteen from './newlottiefiles/Supercard-10.json'
+import Fifteen from './newlottiefiles/Supercard-11.json'
 
-document.getElementById("3rd-div-next-btn").addEventListener("click", thirdDivMove);
-document.getElementById("last-next-btn").addEventListener("click", lastNextBtn);
 
-function lastNextBtn() {
-    console.log("last next button clicked")
-    if (window?.location?.search === "?device=ios") {
-        window.location.href = "popclubapp://skiponboarding";
-    }
-
-    if (window?.location?.search === "?device=android") {
-        window.JSInterface.skip()
-    }
-}
-
-function handleRewardsBtn() {
-    console.log("Skip Button Clicked")
-    if (window?.location?.search === "?device=ios") {
-        window.location.href = "popclubapp://skiponboarding";
-    }
-
-    if (window?.location?.search === "?device=android") {
-        window.JSInterface.skip()
-    }
-}
-
-var count = 1;
-
-console.log("count", count)
-
-const animations = [FirstDiv1Animation, FirstDiv2Animation, FirstDiv3Animation]
-
-/*
-scan-loop -> 3rd frame
-scan-transition - 2nd frame
-1 -> 1st frame
-*/
-var animation1;
-var animation2;
-var animation3;
-
-function playNextAnimation() {
-    animation1.destroy()
-    animation2 = lottieWeb.loadAnimation({
-        container: document.getElementById('lottieDivFirst'),
-        animationData: animations[1],
-        renderer: 'svg',
-        loop: false,
-        autoplay: true,
-        name: "Demo Animation",
-    });
-
-    animation2.play()
-    animation2.addEventListener('complete', playNextNextAnimation);
-
-    function playNextNextAnimation() {
-        console.log("second complete")
-        animation2.destroy()
-        animation3 = lottieWeb.loadAnimation({
-            container: document.getElementById('lottieDivFirst'),
-            animationData: animations[2],
-            renderer: 'svg',
-            loop: false,
-            autoplay: true,
-            name: "Demo Animation",
-        });
-        animation3.play()
-        document.getElementById('card-visible-toggle').style.visibility = "visible";
-    }
-}
-
-animation1 = lottieWeb.loadAnimation({
-    container: document.getElementById('lottieDivFirst'),
-    animationData: animations[0],
-    renderer: 'svg',
-    loop: false,
-    autoplay: true,
-    name: "Demo Animation",
-});
-
-// 2nd div onwards
-
-var animation2ndDiv = lottieWeb.loadAnimation({
-    container: document.getElementById('lottieDivSecond'),
-    animationData: SecondDivAnimation,
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    name: "Demo Animation",
-});
-
-var animation4thDiv = lottieWeb.loadAnimation({
-    container: document.getElementById('lottieDivFourth'),
-    animationData: FourthDivAnimation,
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    name: "Demo Animation",
-});
-
-var animation3rdDiv = lottieWeb.loadAnimation({
-    container: document.getElementById('lottieDivThird'),
-    animationData: ThirdDivAnimation,
-    renderer: 'svg',
-    loop: false,
-    autoplay: false,
-    name: "Demo Animation",
-    animationSpeed: 0.1
-});
-
-// var animation5thDiv = lottieWeb.loadAnimation({
-//     container: document.getElementById('lottieDivFifth'),
-//     // TODO - replace this with actual animation
-//     animationData: Test,
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: false,
-//     name: "testing",
-// });
-
-animation1.addEventListener('complete', playNextAnimation);
-
-function firstDivMove() {
-    handleFirst()
-    animation2ndDiv.play()
-    count = 2
-    console.log("count", count)
-}
-
-function secondDivMove() {
-    handleSecond()
-    animation4thDiv.play()
-    count = 3
-    console.log("count", count)
-}
-
-function thirdDivMove() {
-    handleThird()
-    count = 5
-    console.log("count", count)
-    // change the animation here
-    playLastAnimation()
-}
-
-function fourthDivMove() {
-    handleFourth()
-    animation3rdDiv.setSpeed(0.5)
-    animation3rdDiv.play()
-    count = 4;
-    console.log("count", count)
-}
-
-function playLastAnimation() {
-    console.log("lastinggggggggg")
-    animation1?.destroy()
-    animation2?.destroy()
-    animation3?.destroy()
-    document.getElementById('lottieDivFirst').style.display = 'none';
-    document.getElementById('lottieDivFifth').style.backgroundColor = 'black';
-    document.getElementById('card-visible-toggle').style.display = "none";
-    document.getElementById('fifth-bottom-div').style.display = "flex";
-    document.getElementById("first").removeEventListener("touchmove", firstDivMove);
-    // play animation
-    const fifth1animation = lottieWeb.loadAnimation({
-        container: document.getElementById('lottieDivFifth'),
-        animationData: FifthDiv1Animation,
+function first() {
+    animation1 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
         renderer: 'svg',
         loop: false,
         autoplay: false,
-        name: "testing",
+        name: "Demo Animation",
+        animationData: One
     });
-    fifth1animation.play()
-    animation3rdDiv.destroy()
-
-    fifth1animation.addEventListener('complete', playFive2animation);
-
-    function playFive2animation() {
-        console.log("complete last last")
-        fifth1animation.destroy()
-        const fifth2animation = lottieWeb.loadAnimation({
-            container: document.getElementById('lottieDivFifth'),
-            animationData: FifthDiv2Animation,
-            renderer: 'svg',
-            loop: true,
-            autoplay: false,
-            name: "testing",
-        });
-        fifth2animation.play()
-    }
-}
-
-console.log({ count })
-
-function enableScrolling() {
-    document.getElementById("maincontainer").style.overflow = "auto";
-    // document.body.style.overflow = "auto";
-}
-
-function disableScrolling() {
-    document.getElementById("maincontainer").style.overflow = "hidden";
-    // document.body.style.overflow = "hidden";
-}
-
-function handleFirst() {
-    enableScrolling();
-    console.log("here")
-    const ele = document.getElementById("second");
-    smoothScrollIntoView(ele, {
-        block: "start", // or "center" or "end"
-        inline: "nearest", // or "center" or "end"
-        behavior: "smooth",
+    animation1.play()
+    animation1.addEventListener('complete', () => {
+        second();
     });
-    disableScrolling();
 }
+first()
 
-function handleSecond() {
-    enableScrolling();
-    const ele = document.getElementById("fourth");
-    smoothScrollIntoView(ele, {
-        block: "start", // or "center" or "end"
-        inline: "nearest", // or "center" or "end"
-        behavior: "smooth",
+function second() {
+    animation1.destroy();
+    animation2 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Two
     });
-    disableScrolling();
-}
-
-function handleThird() {
-    enableScrolling();
-    const ele = document.getElementById("first");
-    smoothScrollIntoView(ele, {
-        block: "start", // or "center" or "end"
-        inline: "nearest", // or "center" or "end"
-        behavior: "auto",
+    animation2.play()
+    animation2.addEventListener('complete', () => {
+        document.getElementById("next-btn").style.visibility = "visible";
+        document.getElementById("next-btn").addEventListener("click", handleNextButton)
     });
-    disableScrolling();
 }
 
-function handleFourth() {
-    enableScrolling();
-    const ele = document.getElementById("third");
-    smoothScrollIntoView(ele, {
-        block: "start", // or "center" or "end"
-        inline: "nearest", // or "center" or "end"
-        behavior: "smooth",
+function handleNextButton() {
+    third()
+}
+
+function third() {
+    animation2.destroy();
+    animation3 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Three
     });
-    disableScrolling();
+    animation3.play()
+    animation3.addEventListener('complete', () => {
+        document.getElementById("next-btn").style.visibility = "hidden";
+        fourth();
+    });
 }
 
+function fourth() {
+    animation3.destroy();
+    animation4 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Four
+    });
+    animation4.play()
+    animation4.addEventListener('complete', () => {
+        fifth()
+    });
+}
+
+function fifth() {
+    animation4.destroy();
+    animation5 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Fifth
+    });
+    animation5.play()
+    animation5.addEventListener('complete', () => {
+        sixth()
+    });
+}
+
+function sixth() {
+    animation5.destroy();
+    animation6 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Sixth
+    });
+    animation6.play()
+    animation6.addEventListener('complete', () => {
+        sixthloader()
+    });
+}
+
+function sixthloader() {
+    animation6.destroy();
+    animation7 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Sixth_Loader
+    });
+    animation7.play()
+    document.getElementById('first-container').addEventListener("click", handleSixthLoaderClick)
+    animation7.addEventListener('complete', () => {
+    });
+}
+
+function handleSixthLoaderClick() {
+    eigth()
+}
+
+function eigth() {
+    animation7.destroy();
+    animation8 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Eight
+    });
+    animation8.play()
+    document.getElementById('first-container').addEventListener("click", handleNineClick)
+    animation8.addEventListener('complete', () => {
+        // nine()
+        console.log("clicked")
+    });
+}
+
+// function nine() {
+//     document.getElementById('first-container').removeEventListener("click", handleSixthLoaderClick)
+//     animation8.destroy();
+//     animation9 = lottieWeb.loadAnimation({
+//         container: document.getElementById('first-container'),
+//         renderer: 'svg',
+//         loop: true,
+//         autoplay: false,
+//         name: "Demo Animation",
+//         animationData: Nine
+//     });
+//     animation9.play()
+//     document.getElementById('first-container').addEventListener("click", handleNineClick)
+//     animation9.addEventListener('complete', () => {
+//         console.log("hello world")
+//     });
+// }
+
+function handleNineClick() {
+    ten();
+}
+
+function ten() {
+    animation8.destroy();
+    animation10 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Ten
+    });
+    animation10.play()
+    animation10.addEventListener('complete', () => {
+        eleven()
+    });
+}
+
+function eleven(){
+    animation10.destroy();
+    animation11 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: true,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Eleven
+    });
+    animation11.play()
+    document.getElementById('first-container').removeEventListener("click", handleNineClick)
+    document.getElementById('first-container').addEventListener("click", handleElevenClick)
+    animation11.addEventListener('complete', () => {
+        handleElevenClick()
+    });
+}
+
+function handleElevenClick(){
+    console.log("eleven clicked")
+    twelve()
+}
+
+function twelve(){
+    animation11.destroy();
+    animation12 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Twelve
+    });
+    animation12.play()
+    animation12.addEventListener('complete', () => {
+        thirteen()
+    });
+}
+
+function thirteen(){
+    animation12.destroy();
+    animation13 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Thirteen
+    });
+    animation13.play()
+    animation13.addEventListener('complete', () => {
+        console.log("thirteen played")
+        document.getElementById("next-btn").style.visibility = "visible";
+        document.getElementById("next-btn").removeEventListener('click', handleNextButton)
+        document.getElementById("next-btn").addEventListener('click', handleThirteenClick)
+    });
+}
+
+function handleThirteenClick(){
+    console.log("button clicked")
+    fourteen()
+}
+
+function fourteen(){
+    document.getElementById("next-btn").style.visibility = "hidden";
+    document.getElementById("next-btn").removeEventListener('click', handleNextButton)
+    document.getElementById("next-btn").removeEventListener('click', handleThirteenClick)
+
+    animation13.destroy();
+    animation14 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Fourteen
+    });
+    animation14.play()
+    animation14.addEventListener('complete', () => {
+        console.log("fourteen played")  
+        document.getElementById("next-btn").style.visibility = "visible";
+        fifteen()
+    });
+}
+
+
+function fifteen(){
+    document.getElementById("next-btn").style.visibility = "hidden";
+    document.getElementById("next-btn").removeEventListener('click', handleNextButton)
+    document.getElementById("next-btn").removeEventListener('click', handleThirteenClick)
+
+    animation14.destroy();
+    animation15 = lottieWeb.loadAnimation({
+        container: document.getElementById('first-container'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: false,
+        name: "Demo Animation",
+        animationData: Fifteen
+    });
+    animation15.play()
+    animation15.addEventListener('complete', () => {
+        console.log("fifteen played")  
+        document.getElementById("next-btn").style.visibility = "visible";
+    });
+}
+
+
+
+
+
+// function handleTenClick(){
+//     ten()
+// }
+
+
+// function ten(){
+//     animation9.destroy();
+//     animation10 = lottieWeb.loadAnimation({
+//         container: document.getElementById('first-container'),
+//         renderer: 'svg',
+//         loop: false,
+//         autoplay: false,
+//         name: "Demo Animation",
+//         animationData: Nine
+//     });
+//     animation10.play()
+//     animation10.addEventListener('complete', () => {
+//         console.log("10000")
+//     }); 
+// }
+
+
+
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/1_yyGehxKhKc.json?updatedAt=1705916939231
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/2_GoL8XAtoF9.json?updatedAt=1705916939618
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/3_yqvysBjlOr.json?updatedAt=1705916939114
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/4_sHOx1Sy9CA.json?updatedAt=1705916939448
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/5_q-xivi4qaQ.json?updatedAt=1705916939490
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/6_WWIakZ0Z3A.json?updatedAt=1705916939310
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/7_70-nmNc_BG.json?updatedAt=1705916939509
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/8_Kho6C8mEtN.json?updatedAt=1705916939506
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/9_EJRnBCf2X-.json?updatedAt=1705916939396
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/10_PtxbVUeZ4j.json?updatedAt=1705916939512
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/11_UynPX7SIh.json?updatedAt=1705916941900
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/12_gsD0eBx4H.json?updatedAt=1705916941957
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/13_PkusgFYLsS.json?updatedAt=1705916942137
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/14_L8A5ht6U0D.json?updatedAt=1705916942353
+// https://ik.imagekit.io/t2vt6tx4m/onboarding/15_wsncK51wwe.json?updatedAt=1705916942392
